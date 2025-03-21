f643d326196bd74c0939a7054bb25ec8654174d2
Bibek Kumar Ray <bi.ray1225@gmail.com>
2025-03-21 20:18:29 +0545

    Initialized frontend project setup

---
+++ client/src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import Login from "./pages/auth/Login.jsx";
+import Signup from "./pages/auth/Signup.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <>
+        <App />
+        <RouterProvider router={router} />
+    </>
+);
